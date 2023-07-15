bd1c333b4ce559f93aee728d7409e9ff8dab77ee
Abiy Jembere <abiyjem99>
2023-07-15 04:07:15 +0300

    combos

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
 int main(void)
 {
-	int m = '0';
-	int n = '0';
-	int o = '0';
+	int n;
+	int m;
+	int o;
 
-	for (n = '0' ; n <= '9' ; n++)
+	for (n = 0 ; n <= 9 ; n++)
 	{
-		for (m = '0' ; m <= '9' ; m++)
+		for (m = 0 ; m <= 9 ; m++)
 		{
-			for (o = '0' ; o <= '9' ; o++)
+			for (o = 0 ; o <= 9 ; o++)
 			{
-				if (!(n == m || m == o || n == o || o < m || m < n || o < n))
+				if (!(n == m || m == o || n == o || m < n || o < m || o < n))
 				{
 					putchar(n + '0');
 					putchar(m + '0');
 					putchar(o + '0');
-					if (!(n == '7' && m == '8' && o == '9'))
+					if (!(n == 7 && m == 8 && o == 9))
 					{
-							putchar(',');
-							putchar(' ');
+						putchar(',');
+						putchar(' ');
 					}
-
 				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
