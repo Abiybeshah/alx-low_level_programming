dbf05647acedce23d15fc9ee3aa17ccd433707ff
Abiy Jembere <abiyjem99>
2023-07-15 03:33:18 +0300

    betty doesn't agree again free

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,8 +21,7 @@ int main(void)
 					putchar(n + '0');
 					putchar(m + '0');
 					putchar(o + '0');
-
-				if (!(n == 7 && m == 8 && o == '9'))
+					if (!(n == 7 && m == 8 && o == 9))
 					{
 							putchar(',');
 							putchar(' ');
