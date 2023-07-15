09f07e4692edda41facea7546db064ca4fb38246
Abiy Jembere <abiyjem99>
2023-07-15 03:22:35 +0300

    betty doesn't agree but

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int m;
+	int n;
+	int o;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		for (m = 0 ; m <= 9 ; m++)
+		{
+			for (o = 0 ; o <= 9 ; o++)
+			{
+				if (!(n == m || m ==o || n == o || o < m || m < n || o < n))
+				{
+					putchar(n + '0');
+					putchar(m + '0');
+					putchar(o + '0');
+					if (!(n == 7 && m == 8 && o == 9))
+					{
+							putchar(',');
+							putchar(' ');
+					}		
+							
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
