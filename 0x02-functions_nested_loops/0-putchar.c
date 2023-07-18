94fc3f9778b2742c3ede0eb9fad5047f04359603
Abiy Jembere <abiyjem99>
2023-07-18 22:22:48 +0300

    alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
 * main - Entry point this program prints __putchar__
 *
@@ -7,7 +8,14 @@
 
 int main(void)
 {
-#include "main.h"
-	printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 	return (0);
 }
