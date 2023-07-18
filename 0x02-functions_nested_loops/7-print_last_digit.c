fbff7b3a3b92e1f5bc69231bc1ff3c6de9374d4d
Abiy Jembere <abiyjem99>
2023-07-18 23:46:09 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_last_digit - Entry point
+* @n: the value being cheked
+* Return: the value of the lset digit
+*/
+
+int print_last_digit(int n)
+{
+	int las;
+	las = n % 10;
+	return (las);
+}
