8b73e8065037ec22460952fe9b48c0d37b6aec24
Abiy Jembere <abiyjem99>
2023-07-19 00:59:37 +0300

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* add - Entry point
+* @i: value for sum
+* @j: value for sum
+* Return: 0 Successful
+*/
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	_putchar(sum + '0');
+	return (0);
+}
