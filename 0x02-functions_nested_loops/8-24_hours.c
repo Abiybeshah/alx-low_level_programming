eb07758156908b42fbc18b7be69b01bf97026b2c
Abiy Jembere <abiyjem99>
2023-07-19 00:13:43 +0300

    this is trial

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* jack_bauer - Entry point
+*
+* Return: 0 Successful
+*/
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0 ; hour < 24 hour++)
+	{
+		for (minute = 0 ; minute < 60 ; minute++)
+		{
+			_putchar(hour + '0');
+			_putchar(':');
+			_putchar(minute + '0');
+			_putchar('\n');
+		}
+	}
+
+}
