2b59d8624df2a181c6efe98d79c65c4ab84c55ef
Abiy Jembere <abiyjem99>
2023-07-19 01:29:57 +0300

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,18 @@ void print_to_98(int n)
 	while (n < 98)
 	{
 		printf("%d", n);
-		printf("\n");
+		printf(" ");
 		n++;
 	}
 	while (n > 98)
 	{
 		printf("%d", n);
-		printf("\n");
+		printf(" ");
 		n--;
 	}
 	if (n == 98)
 	{
 		printf("%d", n);
-		printf("\n");
+		printf(" ");
 	}
 }
