3a878c8772ca227d03bd5984d4787853b70d8508
Abiy Jembere <abiyjem99>
2023-07-19 00:32:11 +0300

    this is trial

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,6 +12,8 @@ void jack_bauer(void)
 	{
 		for (m = 0 ; m <= 9 ; m++)
 		{
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			{
 			for (o = 0 ; o <= 5 ; o++)
 			{
 				for (p = 0 ; p <= 9 ; p++)
@@ -23,6 +25,7 @@ void jack_bauer(void)
 					_putchar(p + '0');
 					_putchar('\n');
 				}
+			}
 			}
 		}
 	}
