940b83017ec5c308499c71fbfd8d24f181c449df
Abiy Jembere <abiyjem99>
2023-07-24 23:53:43 +0300

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - prints a string to stdout
+* @str: the input
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	char length;
+	
+	for (length = str ; str != '\0' ; length++)
+	{
+		_putchar(*str++)'
+		_putchar('\n');
+	}
+}
