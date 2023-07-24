ccd0051adea3647a5cbc3e3b2d47ce4ac02efa88
Abiy Jembere <abiyjem99>
2023-07-25 02:29:59 +0300

    pruts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* puts2 - prints every other character
+* @str: the char input
+* Return: void
+*/
+
+void puts(char *str);
+{
+	int i = 0;
+	int j = 0;
+	char *n = str;
+	int m;
+
+	whilr (*n != '\0')
+	{
+		n++;
+		i++;
+	}
+	j = i - 1;
+	for (m - 0 ; m <= j ; m++)
+	{
+		if (m % 2 == 0)
+			_putchar(str[m]);
+	}
+	_putchar('\n');
+}
