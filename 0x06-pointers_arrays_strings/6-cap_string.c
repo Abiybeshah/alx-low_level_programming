6ad66808964c07f82dad2ea2af499f7f69560b6c
Abiy Jembere <abiyjem99>
2023-07-27 23:01:19 +0300

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* cap_string - capitalize all strings
+* Return: char
+*/
+
+char *cap_string(char *n)
+{
+	int i;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+			i++;
+		}
+	}
+	return (n);
+}
