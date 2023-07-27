60f0bf739ac7b9a651132ef0f7d7b99469aee40b
Abiy Jembere <abiyjem99>
2023-07-28 02:06:11 +0300

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,27 +2,29 @@
 /**
 * leet - encodes the string to 1337
 * @n: is the input of char
-* Return n
+* Return: n
 */
 
 char *leet(char *n)
 {
 	int i;
+	int j;
 	char m[] = "aAeEoOtTlL";
-	int k[] = "4433007711";
+	char k[] = "4433007711";
 
+	i = 0;
+	j = 0;
 	while (n[i] != '\0')
 	{
-		int j;
 		while (j < 10)
 		{
 			if (m[j] == n[i])
 			{
 				n[i] == m[j];
 			}
-			j++
+			j++;
 		}
-		i++
+		i++;
 	}
 	return (n);
 }
