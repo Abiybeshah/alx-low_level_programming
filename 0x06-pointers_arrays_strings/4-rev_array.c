b10d5d6f2900896227aa341d4eba65cbf5bef119
Abiy Jembere <abiyjem99>
2023-07-27 22:40:49 +0300

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* reverse_array - reverse the order of the array
+* @a: input for int pointer
+* @n: input for number of elemets of the array
+* Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int m;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		m = a[i];
+		a[i] = a[n];
+		a[n] = m;
+	}
+}
