ccc1b5cd4f98cf5e03c4e19e78cc4f2b158f9060
Abiy Jembere <abiyjem99>
2023-07-28 02:32:09 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* rot13 - uses the rot 13 encryption
+* @n: the input in char
+* return: n
+*/
+
+char *rot13(char *n)
+{
+	int i;
+	char a[] = "abcdefghijklm";
+	char b[] = "nopqrstuvwxyz";
+	int j;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		j = 0;
+		while (j <= 13)
+		{
+			if (n[i] == a[j] || n[i] == b[j])
+			{
+				n[i] = a[j];
+				n[i] = b[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (n);
+}
