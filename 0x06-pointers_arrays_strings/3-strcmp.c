c5e9069953a46d95a2368afd6a691a3bdd264d97
Abiy Jembere <abiyjem99>
2023-07-27 21:00:25 +0300

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_strcmp - this function prints two strings
+* @s1: input of string
+* @s2: input of string
+* Return: 0 Successful
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	while (s1[i] != '\0' && s2[i] = '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
