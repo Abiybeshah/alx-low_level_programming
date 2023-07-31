8dddab58f25c5982d2299248dfafc1d9323f84c0
Abiy Jembere <abiyjem99>
2023-08-01 01:00:44 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* _strstr - this is a function that locates a substring
+* @haystack: pointer to be checked
+* @needle: pointer to be checked against
+* Return: 
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do 
+			{
+				if (needle[i + 1] == '\0')
+				{
+					return (haystack);
+				}
+				i++;
+				while (haystack[i] == needle[i])
+			}
+		}
+		haystack++;
+	}
+	return ('\0');
+}
