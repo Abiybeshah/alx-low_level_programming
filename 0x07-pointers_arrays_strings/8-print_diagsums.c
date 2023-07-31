0f63d5e6574d195eccc3efaa2a9ecb44d69d3610
Abiy Jembere <abiyjem99>
2023-08-01 01:41:56 +0300

    print diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_diagsums - prints the sum of a square matrix of integes
+* @a: input
+* @size: input
+* Return: void
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum = 0;
+	int sum1 = 0;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sum += a[i];
+		a += size;
+	}
+	a -= size;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sum1 += a[i];
+		a -= size;
+	}
+	printf("%d, %d\n", sum, sum1);
+}
