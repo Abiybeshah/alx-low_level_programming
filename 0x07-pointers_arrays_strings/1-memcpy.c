161f6035b1b75c3f7c818d32e5cecf13b5369cdd
Abiy Jembere <abiyjem99>
2023-07-31 22:42:07 +0300

    mem copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memcpy - copy data form src to dest
+* @dest: input
+* @src: input
+* @n: input
+* Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest);
+}
