4712381239c51c306ee9a72114865fde1c9f7573
Abiybeshah <abiyjem99@gmail.com>
2023-10-03 01:35:56 +0300

    static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - computes the abs val of an int
+* @n: the int to check
+* Return: the abs val of int
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
