23e60dcb46852585925a59607c80f5103db88ec1
Abiybeshah <abiyjem99@gmail.com>
2023-10-03 01:54:13 +0300

    argc argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -32,8 +32,8 @@ int _atoi(char *s)
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
-			{
-				digit = s[i] - '0';
+		{
+			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
@@ -41,8 +41,8 @@ int _atoi(char *s)
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
-			}
-			i++;
+		}
+		i++;
 	}
 
 	if (f == 0)
