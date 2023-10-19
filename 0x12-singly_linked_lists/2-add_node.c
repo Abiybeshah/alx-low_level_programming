74519e59214b408b2c8e8a849bd81f4f73a6d9f8
Abiybeshah <abiyjem99@gmail.com>
2023-10-20 00:49:31 +0300

    push

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
 * add_node - adds a new node at the beginning of a linked list
@@ -20,8 +20,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new)
 		return (NULL);
 
-	new->str = strdup(str);
 	new->len = length;
+	new->str = strdup(str);
 	new->next = *head;
 	*head = new;
 
