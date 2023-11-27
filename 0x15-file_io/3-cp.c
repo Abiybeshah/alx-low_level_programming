c986fb46f11f245a793b8a07fb1588a1ea4bc053
Abiybeshah <abiyjem99@gmail.com>
2023-11-28 00:34:18 +0300

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 
 /**
-* error_file - checks if files can be opened
-* @file_from: file_from
-* @file_to: file to
-* @argv: arg vector
-* Return: no return
+* error_file - checks if files can be opened.
+* @file_from: file_from.
+* @file_to: file_to.
+* @argv: arguments vector.
+* Return: no return.
 */
-
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -24,12 +23,11 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
-* main - checks the code for Holberto School students
-* @argc: num of args
-* @argv: arg vector
-* Return: Always 0
+* main - check the code for ALX students.
+* @argc: number of arguments.
+* @argv: arguments vector.
+* Return: Always 0.
 */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
@@ -43,20 +41,20 @@ int main(int argc, char *argv[])
 	}
 
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0644);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
-
+	nchars = read(file_from, buf, 1024);
+	if (nchars == -1)
+		error_file(-1, 0, argv);
+	nwr = write(file_to, buf, nchars);
+	if (nwr == -1)
+		error_file(0, -1, argv);
 	}
+
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
