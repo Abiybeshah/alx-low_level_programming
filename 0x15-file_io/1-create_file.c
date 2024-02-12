3ac4aa323eff0ec2b6f81d70d72e3518fd8c19b0
Abiybeshah <abiyjem99@gmail.com>
2024-02-13 01:06:06 +0300

    create and append

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* create_file - fun that creates a file
+* @filename: pointer
+* @text_content: content file
+* Return: 1 on success, -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, fi;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+		++i;
+	if (fi == -1)
+		return (-1);
+
+	write(fi, text_content, i);
+
+	return (1);
+
+
+}
