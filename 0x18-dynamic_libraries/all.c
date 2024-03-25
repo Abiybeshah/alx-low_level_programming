a39bf7caa1de659753dafcf0a9a6ee43229632dd
Abiybeshah <abiyjem99@gmail.com>
2024-03-26 02:54:12 +0300

    operations

--- 0x18-dynamic_libraries/all.c
+++ 0x18-dynamic_libraries/all.c
@@ -1,7 +1,7 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
-* add - add two integers
+* add - adds two integers
 * @a: first unt
 * @b: second int
 * Return: addition
@@ -11,3 +11,58 @@ int add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+* sub - subtracts two integers
+* @a: first unt
+* @b: second int
+* Return: addition
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* add - multiplies two integers
+* @a: first unt
+* @b: second int
+* Return: addition
+*/
+
+int mul(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* div - divides two integers
+* @a: first unt
+* @b: second int
+* Return: division
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - modulus two integers
+* @a: first unt
+* @b: second int
+* Return: addition
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+* add - add two integers
+* @a: first unt
+* @b: second int
+* Return: addition
+*/
