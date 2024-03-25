fe2f9654922b32626695e91e8667bc8aeca8fdb4
Abiybeshah <abiyjem99@gmail.com>
2024-03-26 01:46:28 +0300

    libdynamic

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strlen - returns the len of a string
+* @s: string
+* Return: len
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	return (i);
+}
